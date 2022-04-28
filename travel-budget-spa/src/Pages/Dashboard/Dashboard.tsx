39877f574947e58cbed0c9c016870d3e3bd652f4
Cecilie Urdahl Fossum <ceciliefossum@gmail.com>
2022-04-28 15:39:23 +0200

    Calculating total balance from statements

--- travel-budget-spa/src/Pages/Dashboard/Dashboard.tsx
+++ travel-budget-spa/src/Pages/Dashboard/Dashboard.tsx
@@ -3,11 +3,17 @@ import './Dashboard.css';
 import { statementsTemp, balancesTemp } from "../../_constants/Constants";
 import Balance from "../../shared/Balance";
 import { IBalance, IStatement } from "../../_interfaces/Interfaces";
+import { BalanceType, StatementType } from "../../_interfaces/Enums";
 
 const Dashboard = () => {
+    const money = statementsTemp.reduce((amount: number, statement: IStatement) =>
+        statement.type === StatementType.Expence ? amount - statement.amount : amount + statement.amount, 0
+    );
+
     return (
         <div className="dashboard-container">
             <div className="balances-container">
+                <Balance balance={{amount: money, type: BalanceType.AccountBalance}} />
                 {balancesTemp.map((balance: IBalance, index: number) => (
                     <Balance key={index.toString()} balance={balance} />
                 ))}
--- travel-budget-spa/src/_Constants/Constants.tsx
+++ travel-budget-spa/src/_Constants/Constants.tsx
@@ -3,7 +3,6 @@ import { IBalance, IStatement } from "../_interfaces/Interfaces";
 
 export const balancesTemp: IBalance[] = [
     { type: BalanceType.TodaysBalance, amount: 100 },
-    { type: BalanceType.AccountBalance, amount: 1000 },
     { type: BalanceType.PastDaysBalance, amount: -50 },
 ];
 
@@ -16,4 +15,6 @@ export const statementsTemp: IStatement[] = [
     { title: "Campingplass", type: StatementType.Expence, amount: 125, valuta: "NOK", category: StatementCategory.CampingSpot },
     { title: "Campingplass", type: StatementType.Expence, amount: 125, valuta: "NOK", category: StatementCategory.CampingSpot },
     { title: "Campingplass", type: StatementType.Expence, amount: 125, valuta: "NOK", category: StatementCategory.CampingSpot },
+    { title: "Campingplass", type: StatementType.Expence, amount: 125, valuta: "NOK", category: StatementCategory.CampingSpot },
+    { title: "Campingplass", type: StatementType.Income, amount: 2000, valuta: "NOK", category: StatementCategory.Income },
 ];
