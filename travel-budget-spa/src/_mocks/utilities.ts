ff9ae669fb353e6a5c297077dd373e704373f378
Cecilie Urdahl Fossum <ceciliefossum@gmail.com>
2022-05-01 12:27:21 +0200

    Added tests for functions to calculate daily budget and todays balance

--- travel-budget-spa/src/_mocks/utilities.ts
+++ travel-budget-spa/src/_mocks/utilities.ts
@@ -1,19 +1,132 @@
 import { TransactionCategory, TransactionType } from "../_interfaces/Enums";
-import { ITransaction } from "../_interfaces/Interfaces";
+import { IBudgetPeriod, ITransaction } from "../_interfaces/Interfaces";
 
-export const transactionsMock: ITransaction[] = [
+const today = new Date();
+
+const oneDayAgo = new Date();
+oneDayAgo.setDate(oneDayAgo.getDate() - 1);
+
+const twoDaysAgo = new Date();
+twoDaysAgo.setDate(twoDaysAgo.getDate() - 2);
+
+const tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
+
+export const transactionsMockAccountBalance50: ITransaction[] = [
     {
         type: TransactionType.Income,
         amount: 100,
         valuta: 'NOK',
         category: TransactionCategory.Income,
-        date: new Date()
+        date: today
     },
     {
         type: TransactionType.Expence,
         amount: 50,
         valuta: 'NOK',
         category: TransactionCategory.DailyExpence,
-        date: new Date()
+        date: today
+    },
+];
+
+
+export const transactionsMockAccountBalanceNegative50: ITransaction[] = [
+    {
+        type: TransactionType.Expence,
+        amount: 100,
+        valuta: 'NOK',
+        category: TransactionCategory.DailyExpence,
+        date: today
+    },
+    {
+        type: TransactionType.Income,
+        amount: 50,
+        valuta: 'NOK',
+        category: TransactionCategory.Income,
+        date: today
+    },
+];
+
+export const transactionsMockAccountBalanceWithFuelExpence50: ITransaction[] = [
+    {
+        type: TransactionType.Expence,
+        amount: 100,
+        valuta: 'NOK',
+        category: TransactionCategory.DailyExpence,
+        date: today
+    },
+    {
+        type: TransactionType.Income,
+        amount: 1000,
+        valuta: 'NOK',
+        category: TransactionCategory.Income,
+        date: today
+    },
+    {
+        type: TransactionType.Expence,
+        amount: 200,
+        valuta: 'NOK',
+        category: TransactionCategory.Fuel,
+        date: today
+    },
+];
+
+export const budgetPeriodDailyBudgetFourDaysTwoDaysLeft: IBudgetPeriod = {
+    startDate: twoDaysAgo,
+    endDate: tomorrow,
+}
+
+export const transactionsMockDailyBudget250: ITransaction[] = [
+    {
+        type: TransactionType.Income,
+        amount: 1000,
+        valuta: 'NOK',
+        category: TransactionCategory.Income,
+        date: oneDayAgo
+    },
+    {
+        type: TransactionType.Expence,
+        amount: 250,
+        valuta: 'NOK',
+        category: TransactionCategory.DailyExpence,
+        date: oneDayAgo
+    },
+    {
+        type: TransactionType.Expence,
+        amount: 250,
+        valuta: 'NOK',
+        category: TransactionCategory.DailyExpence,
+        date: oneDayAgo
+    },
+];
+
+export const transactionsMockTodaysBalance150: ITransaction[] = [
+    {
+        type: TransactionType.Income,
+        amount: 1000,
+        valuta: 'NOK',
+        category: TransactionCategory.Income,
+        date: oneDayAgo
+    },
+    {
+        type: TransactionType.Expence,
+        amount: 250,
+        valuta: 'NOK',
+        category: TransactionCategory.DailyExpence,
+        date: oneDayAgo
+    },
+    {
+        type: TransactionType.Expence,
+        amount: 250,
+        valuta: 'NOK',
+        category: TransactionCategory.DailyExpence,
+        date: oneDayAgo
+    },
+    {
+        type: TransactionType.Expence,
+        amount: 100,
+        valuta: 'NOK',
+        category: TransactionCategory.DailyExpence,
+        date: today
     },
 ];
